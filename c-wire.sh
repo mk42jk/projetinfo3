4da6ccdd419caf3131b96245171204603d0a0991
JK l'ingénieur <jeremie.konda@outlook.fr>
2024-12-14 22:10:35 +0100

    Update c-wire.sh
    Ajout de la fonction validation , début de fonction

--- c-wire.sh
+++ c-wire.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Fonction pour l'affichage de l'aide
-print_help(){
+Affiche_aide(){
      echo "Fonction d'aide exécutée."
      echo "L'utilisation: $0 <Fichier_Données> <Type_de_station> <type_de_consommateur> [ID_Centrale] [-h]"
      echo "Options:" #une ligne pour expliquer chaque argument 
@@ -13,18 +13,45 @@ print_help(){
      exit 0
 }
 
+#Fonctions pour vérifier les arguments 
+validation_arguments() {
+     #Si -h est dans les arguments
+     for arg in "$@"; do
+          if [[ "$arg" == "-h" ]]; then
+               Affiche_aide
+          fi
+     done
+
+     #Vérification du nombre d'arguments (4 minimum)
+     if [[ $# -lt 3 ]]; then #Nombre d'Arguments strictement inférieurs à 3
+          echo "Erreur: Nombre d'arguments insuffisants."
+          affiche_aide
+     fi
+
+     #Initialisation du nom des arguments
+     local Fichier_donnees
+
+
+
+
+
+
+
+
+
+
 temps_execution () {
     #Enregistre le temps au début de l'exécution
-    local start_time=$(date +%s)
+    local debut_temps=$(date +%s)
 
     #Enregistre le temps de fin d'exécution
-    local end_time=$(date +%s)
+    local fin_temps=$(date +%s)
 
     #Calcul le temps total 
-    local execution_time=$((end_time-start_time))
+    local execution_temps=$((fin_temps-debut_temps))
 
     #Affiche le temps
-    echo "Temps d'exécution total : $execution_time secondes."
+    echo "Temps d'exécution total : $execution_temps secondes."
     exit 0
 }
 
