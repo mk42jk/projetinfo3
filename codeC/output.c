8e1ab42c74eefbf629ec42bb2f1eecb74f3e7250
HoucineJib <jibax95150@gmail.com>
2024-12-15 20:39:13 +0100

    v2

--- codeC/output.c
+++ codeC/output.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../include/output.h"
+#include "output.h"
 
 void ecrire_donnees(FILE* f, const NoeudAVL* racine) {
     if (!f || !racine) return;
