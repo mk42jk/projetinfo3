a508f79412368e83b8fb5c94e8aeee3221a157da
HoucineJib <jibax95150@gmail.com>
2024-12-15 21:03:14 +0100

    v4

--- codeC/main.c
+++ codeC/main.c
@@ -1,15 +1,21 @@
+// Fichier : main.c
+// Description : Programme principal de gestion du réseau électrique
+// Lit les données d'entrée, les traite et génère un rapport de sortie
+
 #include <stdio.h>
 #include <stdlib.h>
-#include "input.h"
-#include "output.h"
-#include "avl.h"
+#include "../include/input.h"
+#include "../include/output.h"
+#include "../include/avl.h"
 
 int main(int argc, char* argv[]) {
+    // Vérification des arguments de la ligne de commande
     if (argc < 4) {
         fprintf(stderr, "Usage: %s <fichier_csv> <type_station> <type_consommateur> [id_centrale]\n", argv[0]);
         return 1;
     }
 
+    // Initialisation des paramètres de traitement
     Parametres params;
     params.type_station = (TypeStation)atoi(argv[2]);
     params.type_consommateur = (TypeConsommateur)atoi(argv[3]);
@@ -20,13 +26,19 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    // Création de l'arbre AVL vide
     NoeudAVL* racine = NULL;
+
+    // Traitement du fichier d'entrée et construction de l'arbre
     if (traiter_fichier_entree(argv[1], &racine, &params) != 0) {
         fprintf(stderr, "Erreur lors du traitement du fichier\n");
         return 1;
     }
 
+    // Génération du rapport de sortie
     ecrire_donnees(stdout, racine);
+
+    // Libération de la mémoire
     liberer_avl(racine);
     return 0;
 }
